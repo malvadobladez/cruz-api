bd6d977e92aa915b73838e3695b9f9c6e0716c9a
Rick Cruz (Malvado) <rick@rickcruz.com>
2026-01-27 20:16:14 -0500

    enviorment update

--- api/migrations/env.py
+++ api/migrations/env.py
@@ -33,7 +33,7 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 #target_metadata = None
-from db.models import Base
+from api.db.models import Base
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
